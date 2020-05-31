8603a21deb8ad951aedfdac585ad63dfa3ee5d8c
Mary Mary Jaimun <tuespilot@Nikoms-MacBook-Pro.local>
2020-05-31 23:37:40 +0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Mary Mary Jaimun on 31/5/2563 BE.
+//  Copyright © 2563 Nikom Jaimun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
